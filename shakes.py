660f0fc33748f317cbbb2ac08aa01a5ec88c4b88
kevinlmadison <coolklm121@gmail.com>
2016-11-14 17:57:54 -0500

    Add shakes.py expanation text

---
+++ shakes.py
@@ -0,0 +1,7 @@
+# This script takes a given string and 
+# continuously generates a random string
+# until the given string and random string
+# match. The script will also rank each
+# random string according to how closely it
+# matches the given string.
+
